3a378b88cc2d7681416a3c5d2525075b565b3d5e
Adam Klibisz <aklibis1@vols.utk.edu>
2019-11-20 12:04:15 -0500

    Sidebar is complete, but it doesn't work

--- finalproj_app/app.R
+++ finalproj_app/app.R
@@ -8,14 +8,16 @@ library(shiny)
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      fileInput("file1", "Choose CSV File",
+      fileInput("file", "Choose CSV File",
                 accept = c(
                   "text/csv",
                   "text/comma-separated-values,text/plain",
                   ".csv")
       ),
-      tags$hr(),
-      checkboxInput("header", "Header", TRUE),
+      
+      numericInput("g1", "Guess 1", 0, min = 0, max = 10000),
+      numericInput("g2", "Guess 2", 0, min = 0, max = 10000),
+      numericInput("g3", "Guess 3", 0, min = 0, max = 10000),
       
       actionButton("go", " ",
                    icon = icon("power-off"))
@@ -27,19 +29,6 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  output$contents <- renderTable({
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, it will be a data frame with 'name',
-    # 'size', 'type', and 'datapath' columns. The 'datapath'
-    # column will contain the local filenames where the data can
-    # be found.
-    inFile <- input$file1
-    
-    if (is.null(inFile))
-      return(NULL)
-    
-    read.csv(inFile$datapath, header = input$header)
-  })
 }
 
 shinyApp(ui, server)
