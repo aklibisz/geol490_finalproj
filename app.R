36eecbca609bf55c6d7d98fc7946a7f3479412c8
Adam Klibisz <aklibis1@vols.utk.edu>
2019-12-08 15:54:16 -0500

    Finalized the sidebar (I hope)

--- app.R
+++ app.R
@@ -6,30 +6,42 @@ library(shiny)
 
 
 ui <- fluidPage(
+  
+  #title for whole page
+  titlePanel("Microbial Decay Analysis"),
+  
+  #area of app for user inputs
   sidebarLayout(
+    
     sidebarPanel(
       
       # Allows user to browse for a CSV file from their local machine
       fileInput("file1", "Choose CSV File",
-                accept = c(
-                  "text/csv",
+                multiple = FALSE,
+                accept = c( "text/csv",
                   "text/comma-separated-values,text/plain",
-                  ".csv")
-      ),
+                  ".csv")),
       
-      # Horizontal line for aesthetics and flow
-      tags$hr(),
+      # user inputs whether the file includes headers such as column names
+      checkboxInput("header", "Column headers?", TRUE),
    
+      # user inputs what character distinguishes data beginning and end
+      radioButtons("sep", "Row separator/delimiter?",
+                   choices = c(Comma = ",",
+                               Semicolon = ";",
+                               Tab = "\t"),
+                   selected = ","),
+      
+      tags$hr(),
+      
       p("Numeric Guesses for Nonlinear Function Solution"),
       
       # Split layout allows all the numeric inputs on one row    
       splitLayout(
-              #### Numeric inputs will be fed to python code to 
-              # serve as guesses for a nonlinear function to 
-              #### create a trendline
-              numericInput("g1", " ", 0),
-              numericInput("g2", " ", 0),
-              numericInput("g3", " ", 0)
+              #### Numeric inputs will be fed to python code to help solve nonlinear functions
+              numericInput("num1", " ", 0),
+              numericInput("num2", " ", 0),
+              numericInput("num3", " ", 0)
       ),
       
       tags$hr(),
